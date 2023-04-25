229d3cb1fb01620003b716d7fc48c958ba66f0f0
sophilophie <sophia@sophiamckell.com>
2023-04-25 12:00:18 -0600

    add typing and clean up home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { map, Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AuthState } from '../common/state/auth/auth.reducer';
+import { AppState } from '../common/state/state';
+import { User } from '../common/types/user';
 
 @Component({
   selector: 'rspui-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  isLoggedIn$: Observable<boolean> = this.store.select((state: any) => state.auth)
-    .pipe(map((state: AuthState) => state.isLoggedIn));
+  private subscription: Subscription | undefined;
+
+  isLoggedIn: boolean = false;
+  user: User | null = null;
+
+  authState$: Observable<AuthState> = this.store.select((state: AppState | {}) => (state as AppState).auth);
 
   constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.subscription = this.authState$.subscribe((authState: AuthState) => {
+      this.user = authState.user;
+      this.isLoggedIn = authState.isLoggedIn;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
 
 }
